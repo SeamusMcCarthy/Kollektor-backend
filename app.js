15034259e29b16cb7d4d824df07983dbdcb687d1
Seamus McCarthy <47305550+spoderman01@users.noreply.github.com>
2022-03-29 22:48:11 +0100

    Changed default port

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.llhnv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(app.listen(5000))
+  .then(app.listen(process.env.PORT || 5000))
   .catch((err) => {
     console.log(err.message);
   });
